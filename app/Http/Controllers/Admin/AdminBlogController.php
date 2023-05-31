27ad4ef3728c795096c8625b166372c4255f4533
takaaki <takaaking26@gmail.com>
2023-06-01 07:39:24 +0900

    first commit

--- app/Http/Controllers/Admin/AdminBlogController.php
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -82,6 +82,10 @@ class AdminBlogController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $blog = Blog::findOrFail($id);
+        $blog->delete();
+        Storage::disk('public')->delete($blog->image);
+
+        return to_route('admin.blogs.index')->with('success', 'ブログを削除しました。');
     }
 }
